3d12177d7afac9be9a99b6bd38c06259ce763e4a
Daniel Figuereo <danielafig51@gmail.com>
2022-04-30 10:24:06 -0400

    Created the basic game logic and added an example of a failure point

---
+++ this-example/js/main.js
@@ -0,0 +1,30 @@
+class TestClass {
+  constructor(){
+    this.place = "The moon"
+    this.array = ["The sun","the earth", "the stars"]
+  }
+
+  whereAreWe(){
+    console.log(this)
+  }
+
+  thisUnbound(){
+    document.querySelector(".click-me").addEventListener("click",this.buttonCode)
+  }
+
+  // thisBound(){
+  //   document.querySelector(".click-me").addEventListener("click",this.buttonCode.bind(this))
+  // }
+
+  buttonCode(){
+    console.log("Click")
+    console.log(this)
+  }
+
+}
+
+
+let test = new TestClass
+
+test.thisUnbound()
+// test.thisBound()
